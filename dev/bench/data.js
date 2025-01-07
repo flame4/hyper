810d264764b3006e377b02e90f5d0b92fdbe225e
github-action-benchmark <github@users.noreply.github.com>
2025-01-07 06:29:21 +0000

    add pipeline (cargo) benchmark result for 30f2961e89eb306780d856e6e2c1ee10ffbbafd2

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1736231360719,
+  "repoUrl": "https://github.com/flame4/hyper",
+  "entries": {
+    "pipeline": [
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "distinct": true,
+          "id": "30f2961e89eb306780d856e6e2c1ee10ffbbafd2",
+          "message": "v1.5.2",
+          "timestamp": "2024-12-16T11:16:22-05:00",
+          "tree_id": "68ea849c99f160502371cb06dc26fcc8a79ff738",
+          "url": "https://github.com/flame4/hyper/commit/30f2961e89eb306780d856e6e2c1ee10ffbbafd2"
+        },
+        "date": 1736231355747,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 46045,
+            "range": "± 9352.51",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
